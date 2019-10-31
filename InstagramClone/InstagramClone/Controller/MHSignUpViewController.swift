1dd77351f051eb31756103b89ae79a10bc5072ad
Pied Piper <kasavavava1998@gmail.com>
2019-10-31 22:52:25 +0700

    first commit

---
+++ InstagramClone/InstagramClone/Controller/MHSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MHSignUpViewController.swift
+//  InstagramClone
+//
+//  Created by nguyen manh hung on 10/14/19.
+//  Copyright © 2019 nguyen manh hung. All rights reserved.
+//
+
+import UIKit
+
+class MHSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
